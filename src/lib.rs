f40c9a03b4e2ca104e3206524f52b02465fd804b
sharkmu <csonka.samuel@gmail.com>
2025-09-15 19:43:42 +0200

    implement list, add function

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,27 @@
+use std::fs;
+use std::io::Write;
+
 pub fn help() {
     println!("help")
 }
 
 pub fn list() {
-    println!("list")
+    let contents = fs::read_to_string("data.txt")
+        .expect("No \"data.txt\" file. Try \"tosk add [task]\" to create said file.");
+
+    for (index, line) in contents.lines().rev().enumerate() {
+        println!("{}. {}", index + 1, line);
+    }
 }
 
 pub fn add(task: String) {
-    println!("task to add: {}", task)
+    let mut file = fs::OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open("data.txt")
+        .expect("cannot open file");
+
+    writeln!(file, "{}", task).expect("cannot write to file");
 }
 
 pub fn remove(task: i32) {
