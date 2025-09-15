992875f209402566147e2d79848ee2c1f127aa21
sharkmu <csonka.samuel@gmail.com>
2025-09-15 18:32:13 +0200

    create base
    now only the actual logics and functions have to be written 😂

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+pub fn help() {
+    println!("help")
+}
+
+pub fn list() {
+    println!("list")
+}
+
+pub fn add(task: String) {
+    println!("task to add: {}", task)
+}
+
+pub fn remove(task: i32) {
+    println!("task to remove: {}", task)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
+use std::env;
+use tosk;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        eprintln!("No args specified! Run \"tosk help\" for information regarding usage.");
+        return;
+    }
+
+    let command = &args[1];
+
+    match command.as_str() {
+        "list" => {
+            tosk::list();
+        }
+        "add" => {
+            if args.len() == 3 {
+                let task = &args[2];
+                tosk::add(task.to_string());
+            } 
+            else if args.len() > 3{
+                eprintln!("Too many arguments. If your task contains more than 1 word, put them in \"")
+            } else {
+                eprintln!("Error: No task specified.");
+            }
+        }
+        "rm" => {
+            if args.len() == 3 {
+                let task = &args[2];
+                match task.trim().parse::<i32>() {
+                    Ok(num) => tosk::remove(num),
+                    Err(_) => eprintln!("Not a number!"),
+                }
+            }
+            else if args.len() > 3{
+                eprintln!("You can only delete one task at a time.")
+            } else {
+                eprintln!("Error: No task specified for rm");
+            }
+        }
+        "help" => {
+            tosk::help();
+        }
+        _ => eprintln!("Unknown command: {}, see \"tosk help\" for more information", command),
+    }
 }
