a14b90828e3c7862bf0701abdcd0a2f5b03226e3
sharkmu <csonka.samuel@gmail.com>
2025-09-15 20:36:13 +0200

    implement remove, archive_removed functions

--- src/lib.rs
+++ src/lib.rs
@@ -19,11 +19,39 @@ pub fn add(task: String) {
         .create(true)
         .append(true)
         .open("data.txt")
-        .expect("cannot open file");
+        .expect("Cannot open file");
 
-    writeln!(file, "{}", task).expect("cannot write to file");
+    writeln!(file, "{}", task).expect("Cannot write to file");
 }
 
 pub fn remove(task: i32) {
-    println!("task to remove: {}", task)
+    let contents = fs::read_to_string("data.txt")
+        .expect("No \"data.txt\" file. Try \"task add [task]\" to create said file.");
+
+    let mut lines: Vec<String> = contents.lines().map(|line| line.to_string()).collect();
+
+    let lines_length: i32 = lines.len().try_into().expect("value error");
+    let index_to_remove = ((task - lines_length) * (-1)) as usize;
+
+    if index_to_remove >= lines.len() {
+        eprintln!("No such task with that index number!");
+    }
+
+    let removed = lines.remove(index_to_remove);
+    archive_removed(removed);
+
+    let mut file = fs::File::create("data.txt").expect("Cannot open file");
+    for line in lines {
+        writeln!(file, "{}", line).expect("Cannot write to file");
+    }
+}
+
+fn archive_removed(removed: String) {
+    let mut file = fs::OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open("archive.txt")
+        .expect("Cannot open file");
+
+    writeln!(file, "{}", removed).expect("Cannot write to file");
 }
